fac3300dd3184e373625689430e730e9f12146b4
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-01-14 22:14:41 -0600

    Transfer script to separate file

---
+++ rps.js
@@ -0,0 +1,34 @@
+function computerPlay() {
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    const playerMove = playerSelection.toLowerCase();
+    const computerMove = computerSelection.toLowerCase();
+
+    if (playerMove === computerMove) {
+        return "It's a tie";
+    } else if (playerMove === "rock" && computerMove === "paper") {
+        return "You lose! Paper beats rock";
+    } else if (
+        playerMove === "paper" &&
+        computerMove === "scissors"
+    ) {
+        return "You lose! Scissors beats paper";
+    } else if (
+        playerMove === "scissors" &&
+        computerMove === "rock"
+    ) {
+        return "You lose! Rock beats scissors";
+    } else {
+        return `You win, ${playerMove} beats ${computerMove}`;
+    }
+}
