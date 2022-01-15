09cc11f8d0aa92d172c0ded600e3f3f05536e97a
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-01-15 01:14:52 -0600

    Add event listeners to begin the game

--- rps.js
+++ rps.js
@@ -32,3 +32,45 @@ function playRound(playerSelection, computerSelection) {
         return `You win, ${playerMove} beats ${computerMove}`;
     }
 }
+
+function createComputerImg(computerMove){
+    let computerImg = document.createElement("img");
+    if (computerMove === "rock") {
+        computerImg.setAttribute("src", "./images/002-rock.png")
+    } else if (computerMove === "paper") {
+        computerImg.setAttribute("src", "./images/001-tissue-paper.png")
+    } else {
+        computerImg.setAttribute("src", "./images/003-scissor-tool.png")
+    }
+    return computerImg;
+}
+
+function createComputerWeapon (computerMove) {
+    let computerImg = createComputerImg(computerMove);
+    let computerMoveName = document.createElement("h2");
+    computerMoveName.textContent = computerMove;;
+    let computerWeapon = document.createElement("div");
+    computerWeapon.classList.add('weapon');
+    computerWeapon.appendChild(computerImg);
+    computerWeapon.appendChild(computerMoveName);
+
+    return computerWeapon;
+
+}
+
+
+const playerOptions = document.querySelectorAll(".options .weapon");
+playerOptions.forEach((option) => {
+    option.addEventListener("click", () => {
+        let computerWeapon = document.querySelector("#computer-selection .weapon");
+        let computerDiv = document.getElementById("computer-selection");
+        if (computerWeapon) computerDiv.removeChild(computerWeapon);
+        let playerMove = option.id;
+        let computerMove = computerPlay();
+        
+        computerDiv.appendChild(createComputerWeapon(computerMove));
+        
+    });
+});
+// let computerContainer = document.getElementById("computer-selection");
+// computerContainer.appendChild(createComputerWeapon(computerMove));
