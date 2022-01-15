6f68047b558462dcf8c436f4b18967ea1fb67717
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-01-15 01:54:14 -0600

    Format files for consistent spacing and indentation

--- rps.js
+++ rps.js
@@ -18,60 +18,54 @@ function playRound(playerSelection, computerSelection) {
         return "It's a tie";
     } else if (playerMove === "rock" && computerMove === "paper") {
         return "You lose! Paper beats rock";
-    } else if (
-        playerMove === "paper" &&
-        computerMove === "scissors"
-    ) {
+    } else if (playerMove === "paper" && computerMove === "scissors") {
         return "You lose! Scissors beats paper";
-    } else if (
-        playerMove === "scissors" &&
-        computerMove === "rock"
-    ) {
+    } else if (playerMove === "scissors" && computerMove === "rock") {
         return "You lose! Rock beats scissors";
     } else {
         return `You win, ${playerMove} beats ${computerMove}`;
     }
 }
 
-function createComputerImg(computerMove){
+function createComputerImg(computerMove) {
     let computerImg = document.createElement("img");
 
     if (computerMove === "rock") {
-        computerImg.setAttribute("src", "./images/002-rock.png")
+        computerImg.setAttribute("src", "./images/002-rock.png");
     } else if (computerMove === "paper") {
-        computerImg.setAttribute("src", "./images/001-tissue-paper.png")
+        computerImg.setAttribute("src", "./images/001-tissue-paper.png");
     } else {
-        computerImg.setAttribute("src", "./images/003-scissor-tool.png")
+        computerImg.setAttribute("src", "./images/003-scissor-tool.png");
     }
     return computerImg;
 }
 
-function createComputerWeapon (computerMove) {
+function createComputerWeapon(computerMove) {
     let computerImg = createComputerImg(computerMove);
     let computerMoveName = document.createElement("h2");
     let computerWeapon = document.createElement("div");
-    
-    computerMoveName.textContent = computerMove;;
-    
-    computerWeapon.classList.add('weapon');
+
+    computerMoveName.textContent = computerMove;
+
+    computerWeapon.classList.add("weapon");
     computerWeapon.appendChild(computerImg);
     computerWeapon.appendChild(computerMoveName);
 
     return computerWeapon;
-
 }
 
 let computerMove;
 let playerMove;
 
-
 const computerDiv = document.getElementById("computer-selection");
 const playerOptions = document.querySelectorAll(".options .weapon");
-const gameResult = document.getElementById("result")
+const gameResult = document.getElementById("result");
 
 playerOptions.forEach((option) => {
     option.addEventListener("click", () => {
-        let computerWeapon = document.querySelector("#computer-selection .weapon");
+        let computerWeapon = document.querySelector(
+            "#computer-selection .weapon"
+        );
         if (computerWeapon) computerDiv.removeChild(computerWeapon);
 
         computerMove = computerPlay();
@@ -79,8 +73,8 @@ playerOptions.forEach((option) => {
         computerWeapon = createComputerWeapon(computerMove);
         computerDiv.appendChild(computerWeapon);
 
-        computerWeapon.addEventListener('animationend', () => {
+        computerWeapon.addEventListener("animationend", () => {
             gameResult.textContent = playRound(computerMove, playerMove);
-        })
+        });
     });
 });
